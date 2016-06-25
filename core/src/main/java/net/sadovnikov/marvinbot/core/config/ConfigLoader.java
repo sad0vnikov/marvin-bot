0b83fe63f9b006948d97c9604c2826e3969fa462
Alexander Sadovnikov <alex@sadovnikov.net>
2016-06-25 10:35:46 +0300

    + overriding settings by system properties

--- core/src/main/java/net/sadovnikov/marvinbot/core/config/ConfigLoader.java
+++ core/src/main/java/net/sadovnikov/marvinbot/core/config/ConfigLoader.java
@@ -1,5 +1,8 @@
 package net.sadovnikov.marvinbot.core.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,13 +11,17 @@ import java.util.Properties;
 public class ConfigLoader {
 
     private Properties prop;
+    private Logger logger;
 
     public ConfigLoader(String fileName) throws ConfigException {
 
+        logger = LogManager.getLogger("core-logger");
+        logger.debug(System.getenv());
         prop = new Properties();
 
         try (InputStream inputStream = new FileInputStream(fileName)) {
             prop.load(inputStream);
+            logger.debug("loaded config file " + fileName);
         } catch (IOException e) {
             throw new ConfigException("net.sadovnikov.marvinbot.core.config file " + fileName + "couldn\'t be loaded");
         }
@@ -25,15 +32,21 @@ public class ConfigLoader {
     {
 
         String value = System.getProperty(name); // every config param can be overridden by system property
+        logger.debug("reading property " + name + " from system properties");
 
         if (value == null) {
-            value = System.getenv(name); // or by env param
+            String envName = name.toUpperCase().replace(".", "_");
+            logger.debug("reading property " + envName + " from env");
+            value = System.getenv(envName); // or by env param
         }
 
 
         if (value == null) {
+            logger.debug("reading property " + name + " from config file");
             value = prop.getProperty(name);
         }
+
+        logger.debug("property " + name + " = " + value);
         return value;
     }
 
