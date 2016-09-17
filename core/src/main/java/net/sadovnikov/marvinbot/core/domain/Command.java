8d11430e55563d67313dd2e34bcf454780b5786e
Alexander Sadovnikov <alex@sadovnikov.net>
2016-09-17 14:44:46 +0300

    + action and actionArgs in Command

--- core/src/main/java/net/sadovnikov/marvinbot/core/domain/Command.java
+++ core/src/main/java/net/sadovnikov/marvinbot/core/domain/Command.java
@@ -1,5 +1,8 @@
 package net.sadovnikov.marvinbot.core.domain;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Command {
 
     private String[] args;
@@ -16,5 +19,25 @@ public class Command {
 
     public String[] getArgs() {
         return this.args;
+    }
+
+    public Optional<String> action() {
+        Optional<String> action = Optional.empty();
+        String[] args = getArgs();
+        if (args.length > 0) {
+            action = Optional.of(args[0]);
+        }
+
+        return action;
+    }
+
+    public Optional<String[]> actionArgs() {
+        Optional<String[]> optionArgs = Optional.empty();
+        String[] args = getArgs();
+        if (args.length > 1) {
+            optionArgs = Optional.of(Arrays.copyOfRange(args, 1, args.length));
+        }
+
+        return optionArgs;
     }
 }
---
+++ core/src/test/java/service/CommandParserTest.java
@@ -0,0 +1,59 @@
+package service;
+
+
+
+import net.sadovnikov.marvinbot.core.domain.Command;
+import net.sadovnikov.marvinbot.core.service.CommandParser;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CommandParserTest {
+    
+    @Test
+    public void testNotCommand() {
+        CommandParser parser = new CommandParser("!");
+        assertFalse(parser.getIsCommand("this shouldn't be parsed as a command"));
+        assertFalse(parser.getIsCommand(".and this to"));
+    }
+    
+    @Test
+    public void testWithoutArgs() {
+        CommandParser parser = new CommandParser("!");
+        assertTrue(parser.getIsCommand("!command"));
+        Command command = parser.parse("!command");
+        assertEquals("command", command.getCommand());
+
+        parser = new CommandParser(".");
+        assertFalse(parser.getIsCommand("!command"));
+        assertTrue(parser.getIsCommand(".command"));
+        command = parser.parse(".command");
+        assertEquals("command", command.getCommand());
+    }
+
+    @Test
+    public void testArgs() {
+        CommandParser parser = new CommandParser("!");
+        String cmdString = "!command test-action test-arg-1 test-arg-2";
+        assertTrue(parser.getIsCommand(cmdString));
+        Command command = parser.parse(cmdString);
+        assertEquals("command", command.getCommand());
+        assertArrayEquals(new String[]{"test-action", "test-arg-1", "test-arg-2"}, command.getArgs());
+
+        assertTrue(command.action().isPresent());
+        assertEquals("test-action", command.action().get());
+        assertTrue(command.actionArgs().isPresent());
+        assertArrayEquals(new String[]{"test-arg-1", "test-arg-2"}, command.actionArgs().get());
+
+
+        cmdString = "!command test-action";
+        command = parser.parse(cmdString);
+        assertTrue(command.action().isPresent());
+        assertEquals("test-action", command.action().get());
+        assertFalse(command.actionArgs().isPresent());
+
+        cmdString = "!command";
+        command = parser.parse(cmdString);
+        assertFalse(command.action().isPresent());
+        assertFalse(command.actionArgs().isPresent());
+    }
+}
