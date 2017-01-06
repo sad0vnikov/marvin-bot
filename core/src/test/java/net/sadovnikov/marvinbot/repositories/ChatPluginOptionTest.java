ee128d3f8de658b2d6a4c58c1cc32745b35171bb
Alexander Sadovnikov <alex@sadovnikov.net>
2017-01-06 21:20:56 +0300

    * fixed broken ChatOptionsTests

--- core/src/test/java/net/sadovnikov/marvinbot/repositories/ChatPluginOptionTest.java
+++ core/src/test/java/net/sadovnikov/marvinbot/repositories/ChatPluginOptionTest.java
@@ -7,6 +7,7 @@ import net.sadovnikov.marvinbot.core.db.repository.PluginOption;
 import net.sadovnikov.marvinbot.core.domain.Channel;
 import net.sadovnikov.marvinbot.core.domain.ChannelTypes;
 import net.sadovnikov.marvinbot.core.service.chat.Chat;
+import org.bson.BsonBoolean;
 import org.bson.BsonDocument;
 import org.bson.BsonString;
 import org.bson.codecs.StringCodec;
@@ -29,11 +30,14 @@ public class ChatPluginOptionTest {
         Method testAllQueryMethod = PluginChatOption.class.getDeclaredMethod("makeGetAllQuery");
         testAllQueryMethod.setAccessible(true);
         BsonDocument result = encodingFilterToBsonDoc( (Bson) testAllQueryMethod.invoke(option) );
-        assertEquals(2, result.size());
+        assertEquals(3, result.size());
         assertTrue(result.containsKey("pluginName"));
         assertEquals(new BsonString("someTestPlugin"), result.getString("pluginName"));
         assertTrue(result.containsKey("chatId"));
         assertEquals(new BsonString("someChatId"), result.getString("chatId"));
+        assertTrue(result.containsKey("channel"));
+        assertEquals(new BsonString(ChannelTypes.SKYPE.toString()), result.getString("channel"));
+
     }
 
     @Test
@@ -45,7 +49,7 @@ public class ChatPluginOptionTest {
 
         BsonDocument result = encodingFilterToBsonDoc( (Bson) queryMethod.invoke(option, "someOption") );
 
-        assertEquals(3, result.size());
+        assertEquals(4, result.size());
         assertTrue(result.containsKey("pluginName"));
         assertEquals(new BsonString("someTestPlugin"), result.getString("pluginName"));
 
@@ -54,6 +58,9 @@ public class ChatPluginOptionTest {
 
         assertTrue(result.containsKey("name"));
         assertEquals(new BsonString("someOption"), result.getString("name"));
+
+        assertTrue(result.containsKey("channel"));
+        assertEquals(new BsonString(ChannelTypes.SKYPE.toString()), result.getString("channel"));
     }
 
     protected BsonDocument encodingFilterToBsonDoc(Bson filter) {
